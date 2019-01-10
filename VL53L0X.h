6929bed589429b58a94d8b015b26db8f971ca46f
mhenman <mark.henman@gmail.com>
2019-01-10 16:09:26 -0600

    Added virtual marker so we can sub-class.

--- VL53L0X.h
+++ VL53L0X.h
@@ -105,15 +105,15 @@ class VL53L0X
 
     bool init(bool io_2v8 = true);
 
-    void writeReg(uint8_t reg, uint8_t value);
-    void writeReg16Bit(uint8_t reg, uint16_t value);
-    void writeReg32Bit(uint8_t reg, uint32_t value);
-    uint8_t readReg(uint8_t reg);
-    uint16_t readReg16Bit(uint8_t reg);
-    uint32_t readReg32Bit(uint8_t reg);
+    virtual void writeReg(uint8_t reg, uint8_t value);
+    virtual void writeReg16Bit(uint8_t reg, uint16_t value);
+    virtual void writeReg32Bit(uint8_t reg, uint32_t value);
+    virtual uint8_t readReg(uint8_t reg);
+    virtual uint16_t readReg16Bit(uint8_t reg);
+    virtual uint32_t readReg32Bit(uint8_t reg);
 
-    void writeMulti(uint8_t reg, uint8_t const * src, uint8_t count);
-    void readMulti(uint8_t reg, uint8_t * dst, uint8_t count);
+    virtual void writeMulti(uint8_t reg, uint8_t const * src, uint8_t count);
+    virtual void readMulti(uint8_t reg, uint8_t * dst, uint8_t count);
 
     bool setSignalRateLimit(float limit_Mcps);
     float getSignalRateLimit(void);
